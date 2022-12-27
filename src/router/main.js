2775b2021b18d0c2f34df3647c6b32756a95c733
rybkin <github@rybkin.me>
2022-12-27 17:25:34 +0300

    added home page

--- src/router/main.js
+++ src/router/main.js
@@ -1,9 +1,14 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import GroupsList from "@/pages/groups/GroupsList"
+import HomePage from "@/pages/home/HomePage"
 import TasksList from "@/pages/tasks/TasksList"
 import SubjectsList from "@/pages/subjects/SubjectsList"
 
 const routes = [
+    {
+        path: '/',
+        component: HomePage,
+    },
     {
         path: '/groups',
         component: GroupsList,
